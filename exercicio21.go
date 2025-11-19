eebab05b1c2342457a01b6b0b9683aab030f6690
Duarte <du84arte@gmail.com>
2025-11-19 20:00:31 -0300

    inclusão de exercicio de média simples

---
+++ exercicio21.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func media(num float64) string {
+	if num < 60.00 {
+		return "Reprovado"
+	}
+	return "Aprovado"
+}
+
+func main() {
+
+	var mediaGeral float64
+
+nota1 := 60.00
+nota2 := 72.50
+nota3 := 35.50
+nota4 := 53.50
+
+mediaGeral = (nota1+nota2+nota3+nota4)/4
+
+fmt.Printf("A sua média foi %.2f, então você foi %s.\n", mediaGeral, media(mediaGeral))
+}
