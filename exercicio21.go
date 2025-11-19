ecbe67a20531b7db66d1bb7df94a6b0469f0dfde
Duarte <du84arte@gmail.com>
2025-11-19 20:24:17 -0300

    inclusão de exercicio de média simples

--- exercicio21.go
+++ exercicio21.go
@@ -13,12 +13,20 @@ func main() {
 
 	var mediaGeral float64
 
-nota1 := 60.00
+/*nota1 := 60.00
 nota2 := 72.50
 nota3 := 35.50
 nota4 := 53.50
 
-mediaGeral = (nota1+nota2+nota3+nota4)/4
+mediaGeral = (nota1+nota2+nota3+nota4)/4*/
+
+notas := [60.00,72.50,35.50,53.50]
+
+for i:=0; i < 5; i++ {
+	notas += i
+}
+
+mediaGeral = notas/4
 
 fmt.Printf("A sua média foi %.2f, então você foi %s.\n", mediaGeral, media(mediaGeral))
 }
