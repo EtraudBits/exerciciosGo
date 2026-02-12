89a0fc4c4b96209fe559dc45e2c1cc7823141e91
duarteoliveira <du84arte@gmail.com>
2026-02-12 20:46:19 -0300

    refactor: add comentarios

--- churrasco/churrasco.go
+++ churrasco/churrasco.go
@@ -8,11 +8,11 @@ func main () {
 	var pessoa int
 
 	fmt.Print("Quantas pessoas vao ao churrasco?")
-	fmt.Scan(&pessoa)
+	fmt.Scan(&pessoa) // ponteiro para ler a quantidade de pessoas (aponta para a variavel pessoa)
 
-	const mediaPorPessoa = 0.5
+	const mediaPorPessoa = 0.5 // constante de 0.5 kg por pessoa tipo float64
 
-	quantidadeCarne := float64(pessoa) * mediaPorPessoa
+	quantidadeCarne := float64(pessoa) * mediaPorPessoa // converte a variavel pessoa para float64 para fazer a multiplicação
 
 	fmt.Printf("a quantidade de carne para %d pessoas no churrasco é: %.2f kg\n", pessoa, quantidadeCarne)
 
