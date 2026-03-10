d7696e5ea71f550842b98e6e1cb4dbf2ff922f4e
duarteoliveira <du84arte@gmail.com>
2026-03-09 22:28:42 -0300

    feat: Leitura de HTML do crowler demostrando EOF (End Of Fire)

---
+++ endoffire/endoffire.go
@@ -0,0 +1,33 @@
+// aqui Go lida com a leitura de caracteres individuais ou blocos de uma pagina até o fim
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+func main() {
+	// simulando o corpo de uma página HTML.
+	leitor := strings.NewReader("<html>Link1, Link2</html>")
+
+	//criamos um buffer (pequeno espaço na RAM) para ler 1 byte por vez
+	p := make([]byte, 1)
+
+	for {
+		_, err := leitor.Read(p) // tenta ler o próximo byte
+		// engenharia: verificamos se chegamos ao fim (Estrategia EOF) primeiro, antes de verificar outros erros.
+		if err == io.EOF {
+			fmt.Println("\n -- Fim da Leitura: Página processada com sucesso --")
+			break // sai do loop de leitura de forma limpa, indicando que a página foi processada com sucesso
+		}
+		// verificamos se houve um erro REAL (ex.: corrupção de dados).
+		if err != nil {
+			fmt.Printf("Erro catastrofico na leitura: %v\n", err)
+			return
+		}
+		// se err foi nil, processamos o byte lido (neste caso, apenas imprimimos o caractere)
+		fmt.Printf("%c", p[0])
+	}
+}
